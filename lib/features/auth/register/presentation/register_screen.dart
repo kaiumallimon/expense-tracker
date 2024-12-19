cbcd920e36f7f665835148f141e63e59701fa4e2
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-20 01:19:45 +0600

    register screen modifications

--- lib/features/auth/register/presentation/register_screen.dart
+++ lib/features/auth/register/presentation/register_screen.dart
@@ -1,7 +1,15 @@
 import 'package:expense_tracker/common/widgets/custombutton.dart';
+import 'package:expense_tracker/features/auth/register/logics/register_state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quickalert/quickalert.dart';
+
+import '../../login/presentation/login_screen.dart';
+import '../logics/register_bloc.dart';
+import '../logics/register_event.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -55,15 +63,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      // Perform registration logic
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Registration Successful!')),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme;
@@ -78,128 +77,296 @@ class _RegisterScreenState extends State<RegisterScreen> {
     ));
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 70,
-                    decoration: BoxDecoration(
-                      color: theme.primary.withOpacity(.3),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(15),
-                    child: Center(
-                      child: Icon(CupertinoIcons.person_alt,
-                          size: 35, color: theme.primary),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'Register',
-                    style: TextStyle(
-                        fontSize: 24, fontWeight: FontWeight.bold, height: 1),
-                  ),
-                  const SizedBox(height: 20),
-                  Text('Create an account to get started ->',
-                      style:
-                          TextStyle(fontSize: 16, color: Colors.grey.shade600)),
-                  const SizedBox(height: 30),
-                  TextFormField(
-                    controller: _nameController,
-                    decoration:
-                        _customInputDecoration('Full Name', Icons.person),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your name';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _emailController,
-                    decoration:
-                        _customInputDecoration('Email Address', Icons.email),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      if (!RegExp(r'^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}\$')
-                          .hasMatch(value)) {
-                        return 'Please enter a valid email address';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _phoneController,
-                    decoration:
-                        _customInputDecoration('Phone number', Icons.phone),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your phone number';
-                      }
+        child: BlocListener<RegisterBloc, RegisterState>(
+          listener: (context, state) {
+            // Handle RegisterLoading state
+            if (state is RegisterLoading) {
+              QuickAlert.show(
+                context: context,
+                type: QuickAlertType.loading,
+                title: 'Loading',
+                text: 'Please wait...',
+                barrierDismissible: false,
+              );
+            }
 
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _locationController,
-                    decoration: _customInputDecoration(
-                        'Location', CupertinoIcons.location_fill),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your Location';
-                      }
+            // Handle RegisterSuccess state
+            if (state is RegisterSuccess) {
+              // Hide the loading dialog
+              Navigator.of(context).pop();
 
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _passwordController,
-                    decoration: _customInputDecoration('Password', Icons.lock),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      if (value.length < 6) {
-                        return 'Password must be at least 6 characters';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  CustomButton(
-                      width: double.infinity,
-                      height: 50,
-                      text: "Sign up",
-                      onPressed: () => _submitForm(),
-                      color: theme.primary,
-                      textColor: theme.onPrimary,
-                      isBordered: false),
-                  const SizedBox(height: 20),
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    const Text('Already have an account?'),
-                    TextButton(
+              QuickAlert.show(
+                context: context,
+                type: QuickAlertType.success,
+                title: 'Success',
+                text: state.message,
+                barrierDismissible: false,
+              );
+
+              // Clear the form controllers
+              _nameController.clear();
+              _emailController.clear();
+              _passwordController.clear();
+              _phoneController.clear();
+              _locationController.clear();
+
+              // Navigate to the LoginScreen
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => LoginScreen(),
+                ),
+              );
+            }
+
+            // Handle RegisterFailure state
+            if (state is RegisterFailure) {
+              // Hide the loading dialog
+              if (Navigator.of(context).canPop()) {
+                Navigator.of(context).pop();
+              }
+
+              QuickAlert.show(
+                context: context,
+                type: QuickAlertType.error,
+                title: 'Error',
+                text: state.message,
+                barrierDismissible: false,
+              );
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton.filled(
                       onPressed: () {
-                        // Navigate to login screen
+                        Navigator.pop(context);
                       },
-                      child: const Text('Login'),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      icon: const Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    )
+                        .animate()
+                        .fade(
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 800),
+                        )
+                        .scaleXY(
+                          begin: 0.9,
+                          end: 1.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 800),
+                        ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Setup your account',
+                      style: TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.bold, height: 1),
+                    )
+                        .animate()
+                        .fade(
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 800),
+                        )
+                        .scaleXY(
+                          begin: 0.9,
+                          end: 1.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 800),
+                        ),
+                    const SizedBox(height: 20),
+                    Text('To get started, continue creating your account',
+                            style: TextStyle(
+                                fontSize: 16, color: Colors.grey.shade600))
+                        .animate()
+                        .fade(
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 800),
+                        )
+                        .scaleXY(
+                          begin: 0.9,
+                          end: 1.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 800),
+                        ),
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      controller: _nameController,
+                      decoration:
+                          _customInputDecoration('Full Name', Icons.person),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your name';
+                        }
+                        return null;
+                      },
+                    )
+                        .animate()
+                        .fade(
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 800),
+                        )
+                        .scaleXY(
+                          begin: 0.9,
+                          end: 1.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 800),
+                        ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration:
+                          _customInputDecoration('Email Address', Icons.email),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        // if (!RegExp(r'^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}\$')
+                        //     .hasMatch(value)) {
+                        //   return 'Please enter a valid email address';
+                        // }
+                        return null;
+                      },
+                    )
+                        .animate()
+                        .fade(
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 800),
+                        )
+                        .scaleXY(
+                          begin: 0.9,
+                          end: 1.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 800),
+                        ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _phoneController,
+                      decoration:
+                          _customInputDecoration('Phone number', Icons.phone),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your phone number';
+                        }
+
+                        return null;
+                      },
+                    )
+                        .animate()
+                        .fade(
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 800),
+                        )
+                        .scaleXY(
+                          begin: 0.9,
+                          end: 1.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 800),
+                        ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _locationController,
+                      decoration: _customInputDecoration(
+                          'Location', CupertinoIcons.location_fill),
+                      obscureText: false,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your Location';
+                        }
+
+                        return null;
+                      },
+                    )
+                        .animate()
+                        .fade(
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 800),
+                        )
+                        .scaleXY(
+                          begin: 0.9,
+                          end: 1.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 800),
+                        ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration:
+                          _customInputDecoration('Password', Icons.lock),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        return null;
+                      },
+                    )
+                        .animate()
+                        .fade(
+                          curve: Curves.easeIn,
+                          duration: const Duration(milliseconds: 800),
+                        )
+                        .scaleXY(
+                          begin: 0.9,
+                          end: 1.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 800),
+                        ),
+                    const SizedBox(height: 20),
+                    CustomButton(
+                        width: double.infinity,
+                        height: 50,
+                        text: "Sign up",
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            String name = _nameController.text.trim();
+                            String email = _emailController.text.trim();
+                            String password = _passwordController.text.trim();
+                            String phone = _phoneController.text.trim();
+                            String location = _locationController.text.trim();
+
+                            BlocProvider.of<RegisterBloc>(context).add(
+                              RegisterSubmitted(
+                                  name: name,
+                                  email: email,
+                                  password: password,
+                                  phone: phone,
+                                  location: location),
+                            );
+                          }
+                        },
+                        color: theme.primary,
+                        textColor: theme.onPrimary,
+                        isBordered: false),
+                  ],
+                )
+                    .animate()
+                    .fade(
+                      curve: Curves.easeIn,
+                      duration: const Duration(milliseconds: 800),
+                    )
+                    .scaleXY(
+                      begin: 0.9,
+                      end: 1.0,
+                      curve: Curves.easeOut,
+                      duration: const Duration(milliseconds: 800),
                     ),
-                  ])
-                ],
               ),
             ),
           ),
