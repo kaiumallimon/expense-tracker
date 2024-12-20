4b08a72aecf35de78ff471016f99c6a18b71fd0a
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-20 20:06:16 +0600

    cubits and blocs add

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:expense_tracker/common/theme/theming.dart';
 import 'package:expense_tracker/features/auth/login/logics/login_bloc.dart';
 import 'package:expense_tracker/features/auth/register/logics/register_bloc.dart';
+import 'package:expense_tracker/features/dashboard/features/add/logics/add_bloc.dart';
+import 'package:expense_tracker/features/dashboard/features/transactions/logics/transactions_bloc.dart';
+import 'package:expense_tracker/features/dashboard/features/transactions/repository/transactions_repository.dart';
 import 'package:expense_tracker/features/startup/splash/presentation/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'features/dashboard/navigation/logics/navigation_cubit.dart';
 
 void main() async {
   // ensure the binding is initialized
@@ -28,12 +33,23 @@ class ExSyncApp extends StatelessWidget {
         BlocProvider<RegisterBloc>(create: (context) => RegisterBloc()),
         // Login bloc
         BlocProvider<LoginBloc>(create: (context) => LoginBloc()),
+
+        // add navigation cubit here
+        BlocProvider<NavigationCubit>(create: (context) => NavigationCubit()),
+
+        // add provider
+        BlocProvider<AddBloc>(create: (context) => AddBloc()),
+
+        //transactions bloc
+
+        BlocProvider<TransactionsBloc>(
+            create: (context) => TransactionsBloc()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'ExSync',
         theme: getTheme(),
-        home: SplashScreen(),
+        home: const SplashScreen(),
       ),
     );
   }
