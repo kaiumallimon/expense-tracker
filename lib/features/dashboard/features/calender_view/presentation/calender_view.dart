58f6842f3f3f6b2a6cb2ae2ed1f10015ea7844c3
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-20 23:43:06 +0600

    update calendar view to display 'Coming soon' message with theme styling

--- lib/features/dashboard/features/calender_view/presentation/calender_view.dart
+++ lib/features/dashboard/features/calender_view/presentation/calender_view.dart
@@ -5,8 +5,11 @@ class CalenderView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Calender View'),
+    // get the theme
+    final theme = Theme.of(context).colorScheme;
+    return Center(
+      child: Text('Coming soon',
+          style: TextStyle(fontSize: 20, color: theme.onSurface)),
     );
   }
 }
