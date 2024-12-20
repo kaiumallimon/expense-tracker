24710f90eaf606707cfc6a193c54876a9c7967f6
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-20 20:04:06 +0600

    transaction repository add

---
+++ lib/features/dashboard/features/transactions/repository/transactions_repository.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class TransactionsRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String collectionName = 'logs';
+
+  // Get all transactions without any filters
+  Future<List<Map<String, dynamic>>> getTransactions(String uid) async {
+    try {
+      final transactions = await _firestore
+          .collection(collectionName)
+          .where('uid', isEqualTo: uid)
+          .orderBy('timestamp', descending: true)
+          .get();
+
+      final List<Map<String, dynamic>> transactionsList = [];
+      for (var element in transactions.docs) {
+        final data = element.data() as Map<String, dynamic>?;
+        if (data != null) {
+          data['id'] = element.id; // Add the id to the data
+          transactionsList.add(data);
+        }
+      }
+
+      return transactionsList;
+    } catch (e) {
+      print('Error in getTransactions: $e');
+      throw Exception('Failed to fetch transactions.');
+    }
+  }
+}
