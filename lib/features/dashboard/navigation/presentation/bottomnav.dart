d66d7ca52ccfb9c00a65df85a4e1fe4455e254a8
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-20 20:00:27 +0600

    material 3 custom navbar add

---
+++ lib/features/dashboard/navigation/presentation/bottomnav.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../logics/navigation_cubit.dart';
+
+class CustomBottomNavbar extends StatelessWidget {
+  CustomBottomNavbar({super.key, required this.selectedIndex})
+      : currentIndex = selectedIndex;
+  final int currentIndex;
+  final int selectedIndex;
+
+  final List<Map<String, String>> items = [
+    {'icon': 'assets/icons/home.png', 'label': 'Home'},
+    {'icon': 'assets/icons/transactions.png', 'label': 'Log'},
+    {'icon': 'assets/icons/add.png', 'label': 'Add'},
+    {'icon': 'assets/icons/calender.png', 'label': 'History'},
+    {'icon': 'assets/icons/person.png', 'label': 'Profile'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: NavigationBar(
+        surfaceTintColor: Theme.of(context).colorScheme.primary,
+        indicatorColor: Theme.of(context).colorScheme.primary,
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        selectedIndex: currentIndex,
+        onDestinationSelected: (int index) {
+          BlocProvider.of<NavigationCubit>(context).changeIndex(index);
+        },
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.history),
+            label: 'Txns',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.calendar_month),
+            label: 'View',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
