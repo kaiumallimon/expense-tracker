4b4f84eec2e1de59fa438b063b44cc3db67d2153
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-20 23:43:25 +0600

    add theme management with ThemeCubit and integrate into app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:expense_tracker/common/theme/theming.dart';
 import 'package:expense_tracker/features/auth/login/logics/login_bloc.dart';
 import 'package:expense_tracker/features/auth/register/logics/register_bloc.dart';
@@ -5,9 +7,8 @@ import 'package:expense_tracker/features/dashboard/features/add/logics/add_bloc.
 import 'package:expense_tracker/features/dashboard/features/transactions/logics/transactions_bloc.dart';
 import 'package:expense_tracker/features/startup/splash/presentation/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'features/dashboard/features/profile/logics/profile_cubit.dart';
+import 'features/dashboard/features/profile/logics/theme_cubit.dart';
 import 'features/dashboard/navigation/logics/navigation_cubit.dart';
 
 void main() async {
@@ -17,38 +18,42 @@ void main() async {
   // initialize firebase
   await Firebase.initializeApp();
 
+  // Initialize the ThemeCubit and load the theme
+  final themeCubit = ThemeCubit();
+  await themeCubit.loadTheme();
+
   // run the app
-  runApp(const ExSyncApp());
+  runApp(ExSyncApp(themeCubit: themeCubit));
 }
 
 class ExSyncApp extends StatelessWidget {
-  const ExSyncApp({super.key});
+  const ExSyncApp({super.key, required this.themeCubit});
+
+  final ThemeCubit themeCubit;
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        // Register bloc
         BlocProvider<RegisterBloc>(create: (context) => RegisterBloc()),
-        // Login bloc
         BlocProvider<LoginBloc>(create: (context) => LoginBloc()),
-
-        // add navigation cubit here
         BlocProvider<NavigationCubit>(create: (context) => NavigationCubit()),
-
-        // add provider
         BlocProvider<AddBloc>(create: (context) => AddBloc()),
-
-        //transactions bloc
-
-        BlocProvider<TransactionsBloc>(
-            create: (context) => TransactionsBloc()),
+        BlocProvider<TransactionsBloc>(create: (context) => TransactionsBloc()),
+        BlocProvider<ProfileCubit>(create: (context) => ProfileCubit()),
+        // Provide the ThemeCubit
+        BlocProvider<ThemeCubit>(create: (context) => themeCubit),
+        
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'ExSync',
-        theme: getTheme(),
-        home: const SplashScreen(),
+      child: BlocBuilder<ThemeCubit, bool>(
+        builder: (context, darkMode) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'ExSync',
+            theme: darkMode ? getDarkTheme() : getTheme(),
+            home: const SplashScreen(),
+          );
+        },
       ),
     );
   }
