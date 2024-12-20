043f4e30faa052eb935392459e42fe7a5f6e6b77
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-20 23:40:39 +0600

    theme update

--- lib/common/theme/theming.dart
+++ lib/common/theme/theming.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// Light theme
 ThemeData getTheme() => ThemeData(
-      // font family
       fontFamily: GoogleFonts.poppins().fontFamily,
-      // color scheme: all the colors used in the app
       colorScheme: const ColorScheme(
           brightness: Brightness.dark,
           primary: Color(0xFF336CFF),
@@ -17,4 +16,21 @@ ThemeData getTheme() => ThemeData(
           onError: Colors.white,
           surface: Colors.white,
           onSurface: Colors.black),
+    );
+
+// dark theme
+ThemeData getDarkTheme() => ThemeData(
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xFF336CFF),
+          onPrimary: Colors.white,
+          secondary: Color(0xFF183856),
+          onSecondary: Colors.white,
+          tertiary: Color(0xFFEF476F),
+          onTertiary: Colors.white,
+          error: Colors.red,
+          onError: Colors.white,
+          surface: Color(0xFF121212),
+          onSurface: Colors.white),
     );
