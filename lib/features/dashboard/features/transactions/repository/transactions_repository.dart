462b719735f2e336cee7c8dddac33e17bf29b440
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-20 23:42:06 +0600

    add method to fetch latest 10 transactions from the repository

--- lib/features/dashboard/features/transactions/repository/transactions_repository.dart
+++ lib/features/dashboard/features/transactions/repository/transactions_repository.dart
@@ -28,4 +28,31 @@ class TransactionsRepository {
       throw Exception('Failed to fetch transactions.');
     }
   }
+
+
+  // get latest 10 transactions
+  Future<List<Map<String, dynamic>>> getLatestTransactions(String uid) async {
+    try {
+      final transactions = await _firestore
+          .collection(collectionName)
+          .where('uid', isEqualTo: uid)
+          .orderBy('timestamp', descending: true)
+          .limit(10)
+          .get();
+
+      final List<Map<String, dynamic>> transactionsList = [];
+      for (var element in transactions.docs) {
+        final data = element.data() as Map<String, dynamic>?;
+        if (data != null) {
+          data['id'] = element.id; // Add the id to the data
+          transactionsList.add(data);
+        }
+      }
+
+      return transactionsList;
+    } catch (e) {
+      print('Error in getLatestTransactions: $e');
+      throw Exception('Failed to fetch transactions.');
+    }
+  }
 }
