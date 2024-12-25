d5a9c2061acdc344b3e0d22dc4c00eca8f3f861c
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-25 21:53:43 +0600

    add CalenderViewEvent class and LoadCalenderView event

---
+++ lib/features/dashboard/features/calender_view/logics/calender_view_event.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+abstract class CalenderViewEvent extends Equatable{
+  const CalenderViewEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoadCalenderView extends CalenderViewEvent{
+  @override
+  List<Object> get props => [];
+}
